4c60ad52b8d2e0b483e3039b182eb28a7379542c
radekg <radek@gruchalski.com>
2017-02-26 21:47:58 +0100

    0.2.0 snapshot

--- project/BuildDefaults.scala
+++ project/BuildDefaults.scala
@@ -1,7 +1,7 @@
 object BuildDefaults {
 
   def buildScalaVersion = sys.props.getOrElse("scala.version", "2.12.1")
-  def buildVersion = "0.1.0"
+  def buildVersion = "0.2.0-snapshot"
   def buildOrganization = "uk.co.appministry"
   def akkaVersion = "2.4.17"
 
